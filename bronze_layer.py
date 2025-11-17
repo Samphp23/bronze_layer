54d6881c7ab2b9d4759eb2f6376a2a232c3fb698
samir97 <106771836+Samphp23@users.noreply.github.com>
2025-11-17 23:54:36 +0530

    Update bronze_layer.py

--- bronze_layer.py
+++ bronze_layer.py
@@ -1,35 +1,25 @@
-from pyspark.sql import SparkSession
-path_1 = "s3://samir-test-demo-small-data-pipeline/datalake/productions.json"
-path_2 = "s3://samir-test-demo-small-data-pipeline/datalake/sales.csv"
-prod_destination = "s3://samir-test-demo-small-data-pipeline/bronze_layer/productions_parquet/"
-sales_destination = "s3://samir-test-demo-small-data-pipeline/bronze_layer/sales_parquet/"
+%idle_timeout 900
+%glue_version 5.0
+%worker_type G.1X
+%number_of_workers 2
 
-spark = SparkSession.builder \
-    .appName("DataMigrationJob") \
-    .getOrCreate()
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+  
+sc = SparkContext.getOrCreate()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+#########################################################################################
+sales_path = "s3://samir-test-demo-small-data-pipeline/datalake/sales.csv"
+production_path = "s3://samir-test-demo-small-data-pipeline/datalake/productions.json"
+############################################################################################
+df_sales = spark.read.format('csv').option('header', True).option('inferSchema', True).option('mode', 'PERMISSIVE').load(sales_path)
+df_productions = spark.read.format('json').option('multiline', True).load(production_path)
 
-df = spark.read.format("json").option("multiline", True).option("mode", "PERMISSIVE").load(path_1)
-df.coalesce(1).write \
-    .mode("overwrite") \
-    .parquet(prod_destination)
-df1 = spark.read.csv(path_2,header=True,inferSchema=True)
-df1.write.mode("overwrite").parquet(sales_destination)
-
-
-
-from pyspark.sql import SparkSession
-path_1 = "s3://samir-test-demo-small-data-pipeline/datalake/productions.json"
-path_2 = "s3://samir-test-demo-small-data-pipeline/datalake/sales.csv"
-prod_destination = "s3://samir-test-demo-small-data-pipeline/bronze_layer/productions_parquet/"
-sales_destination = "s3://samir-test-demo-small-data-pipeline/bronze_layer/sales_parquet/"
-
-spark = SparkSession.builder \
-    .appName("DataMigrationJob") \
-    .getOrCreate()
-
-df = spark.read.format("json").option("multiline", True).option("mode", "PERMISSIVE").load(path_1)
-df.write \
-    .mode("overwrite") \
-    .parquet(prod_destination)
-df1 = spark.read.csv(path_2,header=True,inferSchema=True)
-df1.write.mode("overwrite").parquet(sales_destination)
+df_sales.write.format('parquet').mode('overwrite').save('s3://samir-test-demo-small-data-pipeline/datalake/sales.parquet')
+df_productions.write.format('parquet').mode('overwrite').save('s3://samir-test-demo-small-data-pipeline/datalake/productions.parquet')
