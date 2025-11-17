10eb2543a8d4040547da0f8aeadea44f13692951
samir97 <106771836+Samphp23@users.noreply.github.com>
2025-11-18 00:10:17 +0530

    Update bronze_layer.py

--- bronze_layer.py
+++ bronze_layer.py
@@ -1,21 +1,24 @@
-import sys
-from awsglue.transforms import *
-from awsglue.utils import getResolvedOptions
-from pyspark.context import SparkContext
-from awsglue.context import GlueContext
-from awsglue.job import Job
-  
-sc = SparkContext.getOrCreate()
-glueContext = GlueContext(sc)
-spark = glueContext.spark_session
-job = Job(glueContext)
-#########################################################################################
+from pyspark.sql import SparkSession
+
+# S3 paths
 sales_path = "s3://samir-test-demo-small-data-pipeline/datalake/sales.csv"
 production_path = "s3://samir-test-demo-small-data-pipeline/datalake/productions.json"
-############################################################################################
-df_sales = spark.read.format('csv').option('header', True).option('inferSchema', True).option('mode', 'PERMISSIVE').load(sales_path)
-df_productions = spark.read.format('json').option('multiline', True).load(production_path)
+sales_dest = "s3://samir-test-demo-small-data-pipeline/bronze_layer/sales_parquet/"
+prod_dest = "s3://samir-test-demo-small-data-pipeline/bronze_layer/productions_parquet/"
 
-df_sales.write.format('parquet').mode('overwrite').save('s3://samir-test-demo-small-data-pipeline/datalake/sales.parquet')
-df_productions.write.format('parquet').mode('overwrite').save('s3://samir-test-demo-small-data-pipeline/datalake/productions.parquet')
+# Initialize Spark session
+spark = SparkSession.builder \
+    .appName("DataMigrationJob") \
+    .getOrCreate()
 
+# Read CSV
+df_sales = spark.read.csv(sales_path, header=True, inferSchema=True, mode="PERMISSIVE")
+# Read JSON
+df_productions = spark.read.json(production_path, multiLine=True, mode="PERMISSIVE")
+
+# Write Parquet
+df_sales.write.mode("overwrite").parquet(sales_dest)
+df_productions.write.mode("overwrite").parquet(prod_dest)
+
+# Stop Spark session
+spark.stop()
