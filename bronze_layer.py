ce7482096e69b2a31390cd7bd53e627bcc17d199
samir97 <106771836+Samphp23@users.noreply.github.com>
2025-11-18 02:15:40 +0530

    Update bronze_layer.py

--- bronze_layer.py
+++ bronze_layer.py
@@ -1,32 +1,24 @@
-from awsglue.transforms import *
-from awsglue.utils import getResolvedOptions
-from pyspark.context import SparkContext
-from awsglue.context import GlueContext
-from awsglue.job import Job
+from pyspark.sql import SparkSession
 
-sc = SparkContext.getOrCreate()
-glueContext = GlueContext(sc)
-spark = glueContext.spark_session
-job = Job(glueContext)
+spark = SparkSession.builder \
+    .appName("BronzeLayerETL") \
+    .getOrCreate()
 
 # S3 paths
 sales_path = "s3://samir-test-demo-small-data-pipeline/datalake/sales.csv"
 production_path = "s3://samir-test-demo-small-data-pipeline/datalake/productions.json"
+
 sales_dest = "s3://samir-test-demo-small-data-pipeline/bronze_layer/sales_parquet/"
 prod_dest = "s3://samir-test-demo-small-data-pipeline/bronze_layer/productions_parquet/"
 
 # Read CSV
 df_sales = spark.read.csv(sales_path, header=True, inferSchema=True, mode="PERMISSIVE")
+
 # Read JSON
-df_productions = spark.read.json(production_path, multiLine=True, mode="PERMISSIVE")
+df_productions = spark.read.json(production_path, multiLine=True)
 
 # Write Parquet
 df_sales.write.mode("overwrite").parquet(sales_dest)
 df_productions.write.mode("overwrite").parquet(prod_dest)
 
-# Stop Spark session
 spark.stop()
-
-
-
-
