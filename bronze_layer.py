5bdbc3cdc7259f777ed3a3e4f75f712d2d41b946
samir97 <106771836+Samphp23@users.noreply.github.com>
2025-11-18 13:04:07 +0530

    Update bronze_layer.py

--- bronze_layer.py
+++ bronze_layer.py
@@ -1,9 +1,19 @@
-from pyspark.sql import SparkSession
+%idle_timeout 2880
+%glue_version 5.0
+%worker_type G.1X
+%number_of_workers 5
 
-spark = SparkSession.builder \
-    .appName("BronzeLayerETL") \
-    .getOrCreate()
-
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+  
+sc = SparkContext.getOrCreate()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
 # S3 paths
 sales_path = "s3://samir-test-demo-small-data-pipeline/datalake/sales.csv"
 production_path = "s3://samir-test-demo-small-data-pipeline/datalake/productions.json"
@@ -22,3 +32,4 @@ df_sales.write.mode("overwrite").parquet(sales_dest)
 df_productions.write.mode("overwrite").parquet(prod_dest)
 
 spark.stop()
+
